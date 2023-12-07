03901ba7dd475ca405d63216e942407d1e04f39e
Derek Chavez <frrenose17@yahoo.com>
2023-12-07 15:54:43 -0700

    feat: add percentage methods to round class

--- lib/round.rb
+++ lib/round.rb
@@ -19,11 +19,26 @@ class Round
 
   def number_correct
     correct = 0
-    turns.map do |turn|
-      if turn.correct?
-        correct += 1
-      end
-      return correct
+    turns.each do |turn|
+      correct += 1 if turn.correct?
     end
+    correct
+  end
+
+  def number_correct_by_category(category)
+    correct_turns = turns.select { |turn| turn.correct? && turn.card.category == category }
+    correct_turns.count
+  end
+
+  def percent_correct
+    (number_correct.to_f / turns.count.to_f) * 100
+  end
+
+  def percent_correct_by_category(category)
+    category_turns = turns.select { |turn| turn.card.category == category }
+    return 0.0 if category_turns.empty?
+  
+    correct_turns = category_turns.select(&:correct?)
+    (correct_turns.count.to_f / category_turns.count.to_f) * 100
   end
 end
