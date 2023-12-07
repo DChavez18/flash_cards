6856f15566b5f53fa4754aba31cb3f7188c1b115
Derek Chavez <frrenose17@yahoo.com>
2023-12-07 13:45:01 -0700

    feat: add cards_in_category method in Deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,4 +4,21 @@ class Deck
   def initialize(cards)
     @cards = cards
   end
+
+  def count
+    cards.count
+  end
+
+  def cards_in_category(category)
+    cards.select { |card| card.category == category}
+  end
+
+  # def cards_in_category(category)
+  #   result = []
+
+  #   cards.each do |card|
+  #     result << card if card.category == category
+  #   end
+  #   result
+  # end
 end
